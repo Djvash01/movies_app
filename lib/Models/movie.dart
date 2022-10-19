7d941aa071a97cfa1a0ecb79ccbd45f6321fa0ab
David Yepes <djvash01@gmail.com>
2022-10-19 18:38:21 -0500

    feat: 🎸 get backdrop with full url

--- lib/Models/movie.dart
+++ lib/Models/movie.dart
@@ -40,6 +40,13 @@ class Movie {
       : 'https://i.stack.imgur.com/GNhxO.png';
   }
 
+  get fullBackdropPath {
+
+    return backdropPath != null 
+      ? 'https://image.tmdb.org/t/p/w500$backdropPath'
+      : 'https://i.stack.imgur.com/GNhxO.png';
+  }
+
   factory Movie.fromJson(String str) => Movie.fromMap(json.decode(str));
 
   factory Movie.fromMap(Map<String, dynamic> json) => Movie(
